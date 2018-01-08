d527f217fc62784141b287fec17f15c29b8c5e0a
pcs2216 <coronelsantiagop@gmail.com>
2018-01-08 10:41:01 -0600

    [REF] Etiquetas de remision a otra cosa

--- ventas_blemer/models/datos_remisiones.py
+++ ventas_blemer/models/datos_remisiones.py
@@ -6,7 +6,7 @@ class Datos_ventas(models.Model):
     _name = 'blemer.remisiones'
     _inherit = 'mail.thread'
 
-    name = fields.Char("Remisión", required=True)
+    name = fields.Char("Nota de Remisión: ", required=True)
     x_blemer_cliente = fields.Many2one(
         'res.partner', "Cliente: ", required=True)
     x_blemer_proveedor = fields.Many2one(
@@ -47,57 +47,6 @@ class Datos_ventas(models.Model):
     @api.multi
     def asignar_confirmado(self):
         self.state = 'confirmed'
-
-    @api.multi
-    def generar_seguidores(self):
-        try:
-            for record in self:
-                numero_de_seguidores = len(record.message_follower_ids)
-                if record.x_blemer_cliente != False and record.x_blemer_proveedor != False:
-                    print ("**************** Primer IF")
-                    if numero_de_seguidores != 3:
-                        print ("**************** segundo IF")
-                        id_cliente = record.x_blemer_cliente.id
-                        id_proveedor = record.x_blemer_proveedor.id
-                        modelo = "blemer.remisiones"
-                        id_registro = record.id
-                        ids_subtipo = [1]
-                        # Datos Usuario
-                        ids_subtipos_admin = [1, 2, 3]
-                        id_usuario = record.create_uid.id
-
-                        valores_usuario = {
-                            'res_model': modelo,
-                            'partner_id': id_usuario,
-                            'res_id': id_registro,
-                            'subtype_ids': [(6, 0, ids_subtipos_admin)],
-                        }
-
-                        valores_cliente = {
-                            'res_model': modelo,
-                            'partner_id': id_cliente,
-                            'res_id': id_registro,
-                            'subtype_ids': [(6, 0, ids_subtipo)],
-                        }
-
-                        valores_proveedor = {
-                            'res_model': modelo,
-                            'partner_id': id_proveedor,
-                            'res_id': id_registro,
-                            'subtype_ids': [(6, 0, ids_subtipo)],
-                        }
-
-                        ejecutar_cliente = record.env['mail.followers'].create(
-                            valores_cliente)
-                        print ("**************** ejecutar n1 ****************")
-                        ejecutar_proveedor = record.env['mail.followers'].create(
-                            valores_proveedor)
-                        print ("**************** ejecutar n2 ****************")
-                        ejecutar_usuario = record.env['mail.followers'].create(
-                            valores_usuario)
-                        print ("**************** ejecutar n3 ****************")
-        except Exception as e:
-            print ("******************* Exception ******", e)
 
     # Generar una orden de venta
     @api.one
